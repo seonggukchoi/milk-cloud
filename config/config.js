3cb77c8025218b16d060fb4414aa6a4e6d51e4d6
Milkgun <mrmilkgun@naver.com>
2017-07-06 20:14:01 +0900

    서비스 설정 관련

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const config = {
+  server: {
+    host: 'localhost',
+    port: 8000
+  },
+  service: {
+    name: 'Milk Cloud',
+    version: '1.0.0',
+    root: './cloud_files'
+  }
+}
+
+module.exports = config;
