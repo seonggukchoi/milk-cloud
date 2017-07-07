a550e09333b3774c83b2150a5a2243776570cf2d
Milkgun <mrmilkgun@naver.com>
2017-07-07 11:33:03 +0900

    디렉토리 리스팅 구현

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
 
 const config = require('../config/config');
 
 router.get('/', (req, res, next) => {
-  let fileList = path.join(config.service.root, 'test');
+  let fileList = fs.readdirSync(path.join(config.service.root));
+  res.send(fileList);
+});
 
+router.get('/:dirPath', (req, res, next) => {
+  let dirPath = req.params.dirPath;
+  let fileList = fs.readdirSync(path.join(config.service.root, dirPath));
   res.send(fileList);
 });
 
