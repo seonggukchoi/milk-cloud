0e3e7f57528cea1c355a615118740c258be26a0b
Milkgun <mrmilkgun@naver.com>
2017-07-10 13:56:48 +0900

    파비콘 사용

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const moment = require('moment');
+const favicon = require('serve-favicon');
 
 const config = require('./config/config.js');
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
 
 app.use('/', require('./routes/index'));
 
