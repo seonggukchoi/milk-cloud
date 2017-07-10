2887a7794b31e2b870919062724393caf4c09cb9
Milkgun <mrmilkgun@naver.com>
2017-07-10 13:57:21 +0900

    디렉토리 간 이동 구현

--- routes/index.js
+++ routes/index.js
@@ -16,46 +16,48 @@ router.get('/', (req, res, next) => {
     let fileSize = fileSizeCalc(fileStat.size);
 
     fileList.push({
+      isFile: fileStat.isFile(),
       name: file,
-      size: fileSize,
-      isFile: fileStat.isFile()
+      size: fileSize
     });
   }
 
   res.render('index', {
-    current: '/',
+    current: null,
     files: fileList
   });
 });
 
-router.get('/:dirPath', (req, res, next) => {
-  let dirPath = path.join(config.service.root, req.params.dirPath);
+router.get('/*', (req, res, next) => {
+  let dirPath = path.join(config.service.root, req.params[0]);
   let isDirectory = fs.statSync(dirPath).isDirectory();
 
   if (isDirectory) {
     let fileNames = fs.readdirSync(dirPath);
     let fileList = new Array();
+
     fileList.push({
       name: '..',
       size: null,
       isFile: false
     });
+
     for (let file of fileNames) {
       let fileStat = fs.statSync(path.join(dirPath, file));
       let fileSize = fileSizeCalc(fileStat.size);
       fileList.push({
+        isFile: fileStat.isFile(),
         name: file,
-        size: fileSize,
-        isFile: fileStat.isFile()
+        size: fileSize
       });
     }
 
     res.render('index', {
-      current: req.params.dirPath,
+      current: '/' + req.params[0],
       files: fileList
     });
-  } else if (!isDirectory){
-    res.send(req.params.dirPath);
+  } else if (!isDirectory) {
+    res.send(path.join(config.service.root, req.params[0]));
   }
 });
 
